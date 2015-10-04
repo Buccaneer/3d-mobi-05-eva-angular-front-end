359848fa7f57b032ca0873c97a09bcefa9fb9b3a
Jan Van Braeckel <jan.vanbraeckel.x7253@student.hogent.be>
2015-10-04 14:43:04 +0200

    Initial project setup

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name eva21DayChallengeApp
+ * @description
+ * # eva21DayChallengeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('eva21DayChallengeApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eva21DayChallengeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eva21DayChallengeApp
+ */
+angular.module('eva21DayChallengeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
