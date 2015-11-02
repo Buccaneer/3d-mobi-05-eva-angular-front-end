1a63b408444e666e62055e3f292ca7a72693d30a
Arno Schutijzer <arno.s@outlook.com>
2015-11-02 21:30:59 +0100

    change home.html, change flex on login & register (should probably add a max-width to both..), add some animation to main menu, change recipe layout, add some scss, translate home-page (only in Dutch as of now)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,16 +7,56 @@
  * # MainCtrl
  * Controller of the eva21DayChallengeApp
  */
-app.controller('MainCtrl', ['$localstorage', 'TOKEN', '$scope', 'ChallengeService',
-  function($localstorage, TOKEN, $scope, challengeService) {
+app.controller('MainCtrl', ['$localstorage', 'TOKEN',
+  '$scope', 'ChallengeService', '$interval',
+  function($localstorage, TOKEN, $scope, challengeService, $interval) {
     var tokenObj = $localstorage.getObject(TOKEN);
+    var addDaysInterval;
 
-    var init = function(){
+    var runInterval = function(diffDays) {
+      var interval = $interval(function() {
+        // Increment the Determinate loader
+        $scope.progress.amountOfDays += 1;
+        if ($scope.progress.amountOfDays > 21) {
+          $scope.progress.amountOfDays = 0;
+        }
+
+        console.log($scope.progress.amountOfDays + ' ' + diffDays);
+        if($scope.progress.amountOfDays === diffDays){
+          $interval.cancel(interval);
+        }
+
+      }, 300, 0, true);
+
+    };
+
+    var init = function() {
       //should get more detailed info from the server
-      challengeService.getChallenges().then(function(){
-        console.log(challengeService.challenges);
+      challengeService.getChallenges().then(function() {
+
+        var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+        var firstDate = new Date(challengeService.challenges[challengeService.challenges.length - 1].Date);
+        var secondDate = new Date();
+        var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay)));
+
+        runInterval(diffDays);
       });
     };
+
+    var interval = function(diffDays) {
+      setInterval(animDays(diffDays), 1000);
+    };
+
+    var animDays = function(diffDays) {
+      if ($scope.progress.amountOfDays <= diffDays) {
+        console.log('addDays' + diffDays);
+        $scope.progress.amountOfDays++;
+      } else {
+        console.log('cleared');
+        clearInterval(interval);
+      }
+    };
+
 
     init();
 
@@ -29,7 +69,7 @@ app.controller('MainCtrl', ['$localstorage', 'TOKEN', '$scope', 'ChallengeServic
 
 
     $scope.progress = {
-      amountOfDays: 9,
+      amountOfDays: 0,
       percentagePerDay: 4.7,
     };
 
