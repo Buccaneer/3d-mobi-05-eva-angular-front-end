5a5708dcb708a49ae9b0abe47e1d3c4e454125df
Jasper De Vrient <jasperdevrient@hotmail.com>
2015-12-05 14:43:11 +0100

    Completing the Badges ui.

--- app/scripts/controllers/badges.controller.js
+++ app/scripts/controllers/badges.controller.js
@@ -6,5 +6,18 @@
 	.module('eva21DayChallengeApp')
 	.controller('BadgeCtrl',['$scope','AuthService','UserInfoService', function($scope, auth, UserInfoService) {
 		$scope.userinfo = UserInfoService.userInfo;
+		
+		$scope.title  = '';
+		
+		$scope.userinfo.Badges.forEach(function(elem) {
+			if (elem.contains('level'))
+			{
+				$scope.title = elem;
+			}
+		});
+		
+		$scope.userinfo.Badges = $scope.userinfo.Badges.filter(function(elem) {
+			return !elem.contains('level');
+		});
 	}]);
 })();
