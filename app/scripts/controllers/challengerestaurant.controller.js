00b63f52b679f7d6a36e367957a2a70709b299c2
Arno Schutijzer <arno.s@outlook.com>
2015-12-07 21:22:22 +0100

    add angular-ui-googlemaps and make it work with retrieved long/lat addresses from backend, add some elementary style (needs fixing)

--- app/scripts/controllers/challengerestaurant.controller.js
+++ app/scripts/controllers/challengerestaurant.controller.js
@@ -12,37 +12,57 @@
       $scope.getCurrentPosition = function() {
         var currentPositionPromise = restaurantService.getCurrentPosition();
         currentPositionPromise.then(function(data) {
-          $scope.currentPosition = data.coords;
           console.log(data);
+          $scope.currentPosition = data.coords;
+          $scope.map = { center: { latitude: data.coords.latitude, longitude: data.coords.longitude }, zoom: 15};
         }).catch(function(err) {
           $scope.error = err;
           console.log(err);
         });
       };
 
+
+      /*
+      * find Restaurants inside the radius
+      */
       $scope.findRestaurants = function() {
-        /*
-        Set default values
-        */
-
-        $scope.currentPosition = typeof $scope.currentPosition !== 'undefined' ? $scope.currentPosition : {
-          latitude: 51.053468,
-          longitude: 3.73038
-        };
-
-        $rootScope.loading = true;
         var restaurantPromise = restaurantService.getRestaurants($scope.currentPosition.longitude,
           $scope.currentPosition.latitude, $scope.distance);
 
         restaurantPromise.then(function(data) {
           console.log(data);
-          $rootScope.loading = false;
+          updateMap(data);
         }).catch(function(error) {
+          console.log(error);
           $scope.error = error;
-          $rootScope.loading = false;
+        });
+      };
+
+
+      $scope.clickedMarker = function(marker, event, data){
+        console.log(marker);
+        console.log(event);
+        console.log(data);
+      }
+
+      /*
+      * Update the map with the data retrieved from the API
+      */
+      var updateMap = function(data){
+        var arr = [];
+
+        data.forEach(function(loc){
+          var item = {
+            id: loc.Id,
+            latitude: loc.Latitude,
+            longitude: loc.Longitude
+          }
+          console.log(arr);
+          arr.push(item);
         });
 
-
+        $scope.markers = arr;
+        $scope.map = {center: {longitude: arr[0].longitude, latitude: arr[0].latitude} };
       };
 
 
