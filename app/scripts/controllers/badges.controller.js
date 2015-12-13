89674d24f4b4d0d5cb85541019137bba34a68e9c
Jasper De Vrient <jasperdevrient@hotmail.com>
2015-12-13 14:54:47 +0100

    Progressbar

--- app/scripts/controllers/badges.controller.js
+++ app/scripts/controllers/badges.controller.js
@@ -15,6 +15,37 @@
 				$scope.title = elem;
 			}
 		});
+		var nextXp = 1.0;
+		var nextLevel = '';
+		switch ($scope.title) {
+			case 'level1':
+				nextXp=$scope.userinfo.Points / 5;
+				nextLevel = 'level2';
+				break;
+			case 'level2':
+				nextXp = ($scope.userinfo.Points - 5) / 5;
+				nextLevel = 'level3';
+				break;
+			case 'level3':
+				nextXp = ($scope.userinfo.Points - 10) / 10;
+				nextLevel = 'level4';
+				break;
+			case 'level4':
+				nextXp = ($scope.userinfo.Points - 20) / 12;
+				nextLevel = 'level5';
+				break;
+			case 'level5':
+				nextXp = ($scope.userinfo.Points - 32) / 18;
+				nextLevel = 'level6';
+				break;
+			case 'level6':
+				nextXp = 1;
+				break;
+		}
+		nextXp *= 100;
+		
+		$scope.next = Math.round( nextXp);
+		$scope.nextLevel = nextLevel;
 		
 		$scope.userinfo.Badges = $scope.userinfo.Badges.filter(function(elem) {
 			return !elem.contains('level');
